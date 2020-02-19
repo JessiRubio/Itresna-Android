2d7df60bbc8eb4d93c7c203049721c1e74915f2b
Jon Ander De Castro <45819953+SsrJon@users.noreply.github.com>
2020-02-19 14:24:07 +0100

    php

---
+++ app/src/main/java/com/example/itresna_android/Constants.java
@@ -0,0 +1,13 @@
+package com.example.itresna_android;
+
+public class Constants {
+
+
+    private static final String ROOT_URL = "http://192.168.56.1:8080/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"Login.php";
+
+
+
+}
