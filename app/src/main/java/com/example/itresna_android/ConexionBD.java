e27d13dfbeb8fca77c65bafa1bfc10d4c19186ea
SsrJon <jonanderdecastro@gmail.com>
2020-04-23 12:22:09 +0200

    Login

--- app/src/main/java/com/example/itresna_android/ConexionBD.java
+++ app/src/main/java/com/example/itresna_android/ConexionBD.java
@@ -3,16 +3,19 @@ package com.example.itresna_android;
 public class ConexionBD {
 
 
-    //private static final String ROOT_URL = "http://192.168.56.1:8080/Android/v1/";
+    // En local
+    private static final String ROOT_URL = "http://192.168.56.1:8080/Android/v1/";
 
-    private static final String ROOT_URL = "http://itresna.fptxurdinaga.in/Android/v1/";
+    //En server
+    //private static final String ROOT_URL = "http://itresna.fptxurdinaga.in/Android/v1/";
 
+    public static final String URL_LOGIN = ROOT_URL+"Login.php";
 
-    // operaciones de prueba
+    // Operaciones de prueba
     public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
 
 
-    public static final String URL_LOGIN = ROOT_URL+"Login.php";
+
 
 
 
--- app/src/main/java/com/example/itresna_android/Login.java
+++ app/src/main/java/com/example/itresna_android/Login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -22,37 +23,93 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Login extends AppCompatActivity {
-    Button Entrar;
+    Button btnEntrar;
+    EditText etCod_usuario, etSarbidea;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Entrar = findViewById(R.id.botonEntrar);
-        Entrar.setOnClickListener(new View.OnClickListener() {
+        etCod_usuario= findViewById(R.id.nombreUsuario);
+        etSarbidea=findViewById(R.id.contraseñaUsuario);
+        btnEntrar = findViewById(R.id.botonEntrar);
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                //Intent intent = new Intent(Login.this,PCops.class);
-                //startActivity(intent);
-
 
                 // metedos de prueba para verificar que funcionan insert y select
                 //crear();
                 Login();
-
-
-
-
-
-
 
             }
         });
 
     }
 
+    public void Login(){
 
+        //Datos de prueba
+        //final String cod_usuario="jaime.corrales@gmail.com";
+        //final String sarbidea="jaime123";
+
+        final String cod_usuario= etCod_usuario.getText().toString();
+        final String sarbidea= etSarbidea.getText().toString();
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST,
+                ConexionBD.URL_LOGIN,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            System.out.println(response);
+                            if(!obj.getBoolean("error")){
+
+                                System.out.println(obj.getString("cod_usuario")+" "+obj.getString("tip_usuario")+" "+obj.getString("sarbidea"));
+
+                                //Toast.makeText(getApplicationContext(),obj.getString("cod_usuario"), Toast.LENGTH_LONG).show();
+
+                                Intent intent = new Intent(Login.this, PCops.class);
+                                startActivity(intent);
+
+                            }else{
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("cod_usuario", cod_usuario);
+                params.put("sarbidea", sarbidea);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+        //RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
+
+    }
+
+
+    //Metodo de prueba para insertar datos
+    /*
     public void crear(){
         final int tip_variable= 7;
         final String desc_variable="prueba";
@@ -85,9 +142,6 @@ public class Login extends AppCompatActivity {
                 }
 
         ){
-
-
-
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params =new HashMap<>();
@@ -101,70 +155,6 @@ public class Login extends AppCompatActivity {
         requestQueue.add(stringRequest);
 
     }
+     */
 
-
-    public void Login(){
-
-            final String cod_usuario="jaime.corrales@gmail.com";
-            final String sarbidea="jaime123";
-
-        StringRequest stringRequest = new StringRequest(
-                Request.Method.POST,
-                ConexionBD.URL_LOGIN,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        try {
-                            JSONObject obj = new JSONObject(response);
-                            System.out.println(response);
-                            if(!obj.getBoolean("error")){
-
-                                System.out.println(obj.getString("cod_usuario")+" "+obj.getString("tip_usuario")+" "+obj.getString("sarbidea"));
-
-                                Toast.makeText(getApplicationContext(),obj.getString("cod_usuario"), Toast.LENGTH_LONG
-                                ).show();
-
-                            }else{
-                                Toast.makeText(
-                                        getApplicationContext(),
-                                        obj.getString("message"),
-                                        Toast.LENGTH_LONG
-                                ).show();
-
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(
-                                getApplicationContext(),
-                                error.getMessage(),
-                                Toast.LENGTH_LONG
-                        ).show();
-
-                    }
-                }
-        ){
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("cod_usuario", cod_usuario);
-                params.put("sarbidea", sarbidea);
-                return params;
-            }
-
-        };
-
-        RequestQueue requestQueue= Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
-
-        //RequestHandler.getInstance(this).addToRequestQueue(stringRequest);
-
-    }
 }
