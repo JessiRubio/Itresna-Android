08924265158a303a0937be2e1fb4361b3980afa4
SsrJon <jonanderdecastro@gmail.com>
2020-05-23 14:53:31 +0200

    l

--- app/src/main/java/com/example/itresna_android/ConexionBD.java
+++ app/src/main/java/com/example/itresna_android/ConexionBD.java
@@ -4,10 +4,10 @@ public class ConexionBD {
 
 
     // En local
-    //private static final String ROOT_URL = "http://192.168.56.1:8080/Android/v1/";
+    private static final String ROOT_URL = "http://192.168.56.1:8080/Android/v1/";
 
     //En server
-    private static final String ROOT_URL = "http://itresna.fptxurdinaga.in/Android/v1/";
+    //private static final String ROOT_URL = "http://itresna.fptxurdinaga.in/Android/v1/";
 
     // PHP para hacer el login
     public static final String URL_LOGIN = ROOT_URL+"Login.php";
--- app/src/main/java/com/example/itresna_android/Cop.java
+++ app/src/main/java/com/example/itresna_android/Cop.java
@@ -2,6 +2,24 @@ package com.example.itresna_android;
 
 public class Cop {
 
+    String cod_cop;
+    String cod_esp;
+    String cod_org;
+    String desc_cop;
+    String img_cop;
+    //String ind_cop_graficos;
+
+    public Cop (String cod_cop, String cod_esp, String cod_org, String desc_cop, String img_cop){
+
+        this.cod_cop=cod_cop;
+        this.cod_esp=cod_esp;
+        this.cod_org=cod_org;
+        this.desc_cop=desc_cop;
+        this.img_cop=img_cop;
+
+    }
+
+
     public String getCod_cop() {
         return cod_cop;
     }
@@ -41,23 +59,4 @@ public class Cop {
     public void setImg_cop(String img_cop) {
         this.img_cop = img_cop;
     }
-
-    String cod_cop;
-    String cod_esp;
-    String cod_org;
-    String desc_cop;
-    String img_cop;
-    //String ind_cop_graficos;
-
-    public Cop (String cod_cop, String cod_esp, String cod_org, String desc_cop, String img_cop){
-
-        this.cod_cop=cod_cop;
-        this.cod_esp=cod_esp;
-        this.cod_org=cod_org;
-        this.desc_cop=desc_cop;
-        this.img_cop=img_cop;
-
-    }
-
-
 }
